550dc76c73a9861296b737af6a1b9c4e9444e3c5
Symbioids <123073613+parasnever@users.noreply.github.com>
2024-04-15 08:49:17 +0545

    create the instance and class

---
+++ class_object_example.py
@@ -0,0 +1,19 @@
+# Define class MyFirstClass
+class MyFirstClass():
+    print("Who wrote this?")
+    # Define string variable called index
+    index = "Author-Book"
+    
+    # Define function hand_list()
+    def hand_list(self,philosopher, book):
+        print(MyFirstClass.index)
+    
+        # variable + “ wrote the book: ” + variable
+        print(philosopher + " wrote the book: " + book)
+
+        
+
+# Call function handlist()
+
+whodunnit = MyFirstClass()
+whodunnit.hand_list("laxmit prasad devkota", "Muna Madan")
