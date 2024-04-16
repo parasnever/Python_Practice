f59957ada2828d6b613cc5ba9fa702e41110e8a3
Symbioids <123073613+parasnever@users.noreply.github.com>
2024-04-16 17:51:27 +0545

    changes

--- inheritance_problems.py
+++ inheritance_problems.py
@@ -1,15 +1,46 @@
-# Example 1
+# # Example 1
+
+# class A:
+#     def a(self):
+#         return "Funtion inside A"
+
+# class B:
+#     def a(self):
+#         return "Function inside B"
+
+# class C(B , A):
+#     pass
+
+# c = C()
+# print(c.a())
 
 class A:
-    def a(self):
-        return "Funtion inside A"
+    def d(self):
+        return "Function inside A"
 
 class B:
-    def a(self):
+    def d(self):
         return "Function inside B"
 
-class C(B , A):
+
+class C:
+    def d(self):
+        return "Function inside C"
+
+
+class D(A, B):
+    def d(self):
+        return "Function inside D"
+
+
+class E(B, C):
+    def d(self):
+        return "Function inside E"
+
+
+class F(E,D,C):
     pass
 
-c = C()
-print(c.a())
+f = F()
+print(f.d())
+print(F.mro())
