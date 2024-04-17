de6f9e7ed56162cf4fa29f40431c2b0b63142c1d
Symbioids <123073613+parasnever@users.noreply.github.com>
2024-04-17 23:53:06 +0545

    module

---
+++ my_calender.py
@@ -0,0 +1,13 @@
+# import sys
+# locations = sys.path
+# print(locations)
+# for i in locations:
+#     print(i)
+
+import calendar
+
+# leapday = calender.leapdays(2000, 2050)
+# print(leapday)
+
+is_it_leap = calender.isleap(2036)
+print(is_it_leap)
