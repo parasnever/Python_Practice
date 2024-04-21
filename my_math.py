f2384d91e0e99199c33cf500248b76dff9182939
Symbioids <123073613+parasnever@users.noreply.github.com>
2024-04-21 16:21:03 +0545

    math importing method

---
+++ my_math.py
@@ -0,0 +1,27 @@
+# import math
+# root = math.sqrt(9)
+# print(root)
+
+
+
+# from math import sqrt
+
+# root = sqrt(16)
+# print(root)
+
+
+import math as m
+ 
+cosine = m.cos(0)
+print(cosine)
+
+from math import factorial as f
+factorial_10 = f(10)
+
+print(factorial_10)
+
+from math import *
+
+x = log10(50)
+print(x)
+
