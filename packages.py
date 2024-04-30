9541eac71c03db46e86b792bd3c7c351c817734b
symbioids <parasshresthanever@gmail.com>
2024-04-30 07:43:43 +0545

    pandas packages

--- packages.py
+++ packages.py
@@ -9,3 +9,25 @@ print(b)
 c = np.linspace(0,25,7)
 print(c)
 print(type(c))
+
+
+#pandas packages
+
+import pandas as pd
+
+a = pd.DataFrame({'Animals': ['Dog','Cat','Lion','Cow','Elephant'],
+                  'Sounds':['Barks',"Meow",'Roars','Moo',"Trumpet"]})
+print(a)
+
+print(a.describe())
+
+b = pd.DataFrame({
+    "Letters":['a','b','c','d','e','f'],
+    'Numbers':[12,7,9,3,5,1]
+})
+print(b.sort_values(by ="Numbers"))
+b = b.assign(new_values = b['Numbers'] *3)
+print(b)
+
+#NLTK
+
