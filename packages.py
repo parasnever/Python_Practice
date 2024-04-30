22bc77e4ac1ec6dd9ee8e78409af749e65c97ef2
symbioids <parasshresthanever@gmail.com>
2024-04-30 07:52:44 +0545

    nltk packages

--- packages.py
+++ packages.py
@@ -31,4 +31,23 @@ print(b)
 
 #NLTK
 import nltk
-from nltk.tol
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+
+
+text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+
+# Print statement 1
+print(word_tokenize(text))
+# Print statement 2
+print(nltk.tokenize.sent_tokenize(text))
+
+stopwords = stopwords.words("english")
+new_text = []
+for i in text.split():
+    if i not in stopwords:
+        new_text.append(i)
+
+# Print statement 3
+print(new_text)
+
