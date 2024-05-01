a85d6fe2f5151d0ad2010e3215eb8954b1797051
Symbioids <123073613+parasnever@users.noreply.github.com>
2024-05-01 14:42:33 +0545

    reloads funtion:

--- reloads.py
+++ reloads.py
@@ -2,3 +2,11 @@
 # import sample
 # import sample
 # import sample
+
+import importlib
+import sample
+
+importlib.reload(sample)
+importlib.reload(sample)
+importlib.reload(sample)
+importlib.reload(sample)
