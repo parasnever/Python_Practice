7d30090b13226065474617e8d6844d8947c8dd03
Symbioids <123073613+parasnever@users.noreply.github.com>
2024-05-01 15:08:07 +0545

    complete reload funtion

---
+++ filechanges.py
@@ -0,0 +1,6 @@
+import os
+def print_changes():
+
+    contents = os.listdir(r'/Users/ramki/Desktop/code/python/functional_programming/object_oriented_programming')
+    print("Current directory contents")
+    print(contents)
--- reloads.py
+++ reloads.py
@@ -3,10 +3,31 @@
 # import sample
 # import sample
 
-import importlib
-import sample
 
-importlib.reload(sample)
-importlib.reload(sample)
-importlib.reload(sample)
-importlib.reload(sample)
+# reload funtion
+
+
+# import importlib
+# import sample
+
+# importlib.reload(sample)
+# importlib.reload(sample)
+# importlib.reload(sample)
+# importlib.reload(sample)
+
+# another method 
+
+import importlib
+import filechanges
+
+def changes():
+    try:
+        importlib.reload(filechanges)
+        filechanges.print_changes()
+
+    except:
+        pass
+
+for i in range(5):
+    changes()
+    input("hint to enter reload funtion")
